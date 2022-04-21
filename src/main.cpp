3317b6730ada16efbf222b5cda13b0a0e3e3e8f9
aleksander aleksander <aleksander@alex.al>
2022-04-21 12:31:55 +0200

    initial

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <filesystem>
+
+
+int main()
+{
+    std::cout << "agdadgasdgad" << std::endl;
+}
+
+
+/*REQUIREMENTS
+    Wybieranie folderów
+    Wyswietlanie listy plików i folderow
+    tworzenie i usuwanie plików i folderów
+*/
